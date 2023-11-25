837c8c642893fcffee72815bb91e4dbbbfad3fc4
vitusencia <150796816+vitusencia@users.noreply.github.com>
2023-11-25 14:24:34 +0300

    Added the option to exit the program

--- ThirdHomeWork.java
+++ ThirdHomeWork.java
@@ -1,23 +1,32 @@
 import java.util.Scanner;
-public class Main {
-    
+
+public class ThirdHomeWork {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        while (true) {
-        	    System.out.println("Введи число: ");
-            int i = in.nextInt();
-            if (i == 0) {
-                
+        byte count = 0;
+
+        System.out.println("Для выхода из программы введите три нуля подряд.");
+
+        while (count <= 3) {
+            System.out.println("Введите число: ");
+            int num = in.nextInt();
+
+            if (num == 0) {
+                count++;
+            } else {
+                count = 0;
             }
-            if (i % 3 == 0 && i % 5 == 0) {
+
+            if (num % 3 == 0 && num % 5 == 0) {
                 System.out.println("FizzBazz");
-            } else if (i % 3 == 0) {
+            } else if (num % 3 == 0) {
                 System.out.println("Fizz");
-            } else if (i % 5 == 0) {
+            } else if (num % 5 == 0) {
                 System.out.println("Bazz");
             } else {
-                System.out.println(i);
-            } 
+                System.out.println(num);
+            }
         }
-    }  
+        in.close();
+    }
 }
